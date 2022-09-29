808a26eba1f06834d9876603d976448a572a39f1
hoangduc <hoangtrongduc73@gmail.com>
2022-09-29 15:53:28 +0700

    nodejs MVC

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const { request } = require('express');
+const express = require('express');
+const morgan = require('morgan');
+const { engine } = require('express-handlebars');
+const { extname } = require('path');
+const app = express();
+const port = 8000;
+
+const routes = require('./routes');
+
+app.use
+    (express.static(path.join(__dirname, 'public')));
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
+
+//HTTP logger
+// app.use(morgan('combined'));
+
+//Template engine
+app.engine('.hbs', engine({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+// Route init
+routes(app);
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
